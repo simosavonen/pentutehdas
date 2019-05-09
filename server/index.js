ffa62e97a01f1e6b360df359046fbdeb4de72a8f
Simo Savonen <sipesa@utu.fi>
2019-05-09 19:05:22 +0300

    process.env.PORT issues

--- server/index.js
+++ server/index.js
@@ -105,6 +105,6 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
